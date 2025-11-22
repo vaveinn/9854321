3c973747c65cac10dea0a5d939bd718790ae7a65
vaveinn <vaveinn@outlook.com>
2025-11-22 14:08:57 +0800

    name

--- src/config.ts
+++ src/config.ts
@@ -8,8 +8,8 @@ import type {
 import { LinkPreset } from "./types/config";
 
 export const siteConfig: SiteConfig = {
-	title: "河北科技学院",
-	subtitle: "河北科技学院",
+	title: "河北科技学院网宣部",
+	subtitle: "河北科技学院网宣部",
 	lang: "zh_CN", // Language code, e.g. 'en', 'zh_CN', 'ja', etc.
 	themeColor: {
 		hue: 255, // Default hue for the theme color, from 0 to 360. e.g. red: 0, teal: 200, cyan: 250, pink: 345
